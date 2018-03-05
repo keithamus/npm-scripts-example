459aaf8424c32eba9e997a10a2b4978a0b828b02
francoismassart <francois.massart@gmail.com>
2018-03-05 14:53:40 +0100

    Revert "Demo of the `add-text-banner` tip"
    This reverts commit a11661d3374271980f3a720e3600de126fd9aa8c.

--- add-banner.js
+++
@@ -1,5 +0,0 @@
-var pckgJson = require(process.env.PWD +'/package.json');
-var banner = '/* ' + pckgJson.name + ' - '+ pckgJson.config.banner +' - ' + new Date() + ' */\n';
-
-process.stdout.write(banner);
-process.stdin.pipe(process.stdout);
